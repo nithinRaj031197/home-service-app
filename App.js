63a63ac65ebb1c5f11243fdf807e411e8ae9fc71
nithinRaj031197 <nithinraj.official3@gmail.com>
2024-02-04 00:15:21 +0530

    Authentication using clerk, Bottom Navigation and LoginScreen created

--- App.js
+++ App.js
@@ -1,13 +1,46 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 import Login from "./App/Screens/LoginScreen/Login";
+import { ClerkProvider, SignedIn, SignedOut } from "@clerk/clerk-expo";
+import * as SecureStore from "expo-secure-store";
+import { NavigationContainer } from "@react-navigation/native";
+import TabNavigation from "./App/Navigations/TabNavigation";
 
 export default function App() {
+  const tokenCache = {
+    async getToken(key) {
+      try {
+        return SecureStore.getItemAsync(key);
+      } catch (err) {
+        return null;
+      }
+    },
+    async saveToken(key, value) {
+      try {
+        return SecureStore.setItemAsync(key, value);
+      } catch (err) {
+        return;
+      }
+    },
+  };
+
   return (
-    <View style={styles.container}>
-      <Login />
-      <StatusBar style="auto" />
-    </View>
+    <ClerkProvider tokenCache={tokenCache} publishableKey="pk_test_cGVyZmVjdC1veXN0ZXItNDEuY2xlcmsuYWNjb3VudHMuZGV2JA">
+      <View style={styles.container}>
+        {/* Sign In Component */}
+        <SignedIn>
+          <NavigationContainer>
+            <TabNavigation />
+          </NavigationContainer>
+        </SignedIn>
+
+        {/* SignOut Component */}
+        <SignedOut>
+          <Login />
+        </SignedOut>
+        <StatusBar style="auto" />
+      </View>
+    </ClerkProvider>
   );
 }
 
