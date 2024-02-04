154c9f585c6ad08312843ba1d4242912fce0b451
nithinRaj031197 <nithinraj.official3@gmail.com>
2024-02-04 14:22:20 +0530

    Header component added

--- App.js
+++ App.js
@@ -5,8 +5,14 @@ import { ClerkProvider, SignedIn, SignedOut } from "@clerk/clerk-expo";
 import * as SecureStore from "expo-secure-store";
 import { NavigationContainer } from "@react-navigation/native";
 import TabNavigation from "./App/Navigations/TabNavigation";
+import { useFonts } from "expo-font";
 
 export default function App() {
+  const [fontsLoaded, fontError] = useFonts({
+    outfit: require("./assets/fonts/Outfit-Regular.ttf"),
+    "outfit-medium": require("./assets/fonts/Outfit-Medium.ttf"),
+    "outfit-bold": require("./assets/fonts/Outfit-Bold.ttf"),
+  });
   const tokenCache = {
     async getToken(key) {
       try {
@@ -48,6 +54,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    paddingTop: 20,
+    paddingTop: 30,
   },
 });
---
+++ App/Screens/HomeScreen/Header.jsx
@@ -0,0 +1,79 @@
+import { View, Text, Image, StyleSheet, TextInput } from "react-native";
+import React from "react";
+import { useUser } from "@clerk/clerk-expo";
+import Colors from "../../Utils/Colors";
+import { FontAwesome } from "@expo/vector-icons";
+
+export default function Header() {
+  const { user, isLoading } = useUser();
+  return (
+    user && (
+      <View style={styles.container}>
+        {/* Profile Section */}
+        <View style={styles.profileMainContainer}>
+          <View style={styles.profileContainer}>
+            <Image source={{ uri: user?.imageUrl }} style={styles.userImage} />
+            <View>
+              <Text style={{ color: Colors.WHITE, fontFamily: "outfit" }}>Welcome,</Text>
+              <Text style={{ color: Colors.WHITE, fontSize: 20, fontFamily: "outfit-medium" }}>{user?.fullName}</Text>
+            </View>
+          </View>
+          <FontAwesome name="bookmark-o" size={24} color={Colors.WHITE} />
+        </View>
+        {/* Search bar  */}
+        <View style={styles.searchBarContainer}>
+          <TextInput placeholder="Search" style={styles.textInput} />
+          <FontAwesome name="search" size={24} color={Colors.PRIMARY} style={styles.searchButton} />
+        </View>
+      </View>
+    )
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    paddingTop: 40,
+    backgroundColor: Colors.PRIMARY,
+    borderBottomLeftRadius: 25,
+    borderBottomRightRadius: 25,
+  },
+  profileMainContainer: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  userImage: {
+    width: 45,
+    height: 45,
+    borderRadius: 100,
+  },
+  profileContainer: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 10,
+  },
+  searchBarContainer: {
+    marginTop: 15,
+    display: "flex",
+    flexDirection: "row",
+    gap: 10,
+    marginBottom: 10,
+  },
+  searchButton: {
+    backgroundColor: Colors.WHITE,
+    padding: 10,
+    borderRadius: 8,
+  },
+  textInput: {
+    padding: 7,
+    paddingHorizontal: 16,
+    backgroundColor: Colors.WHITE,
+    borderRadius: 8,
+    width: "85%",
+    fontSize: 16,
+    fontFamily: "outfit",
+  },
+});
